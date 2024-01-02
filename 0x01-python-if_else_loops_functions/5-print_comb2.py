68a511bcf669314de34ec8de3f60e5d64f1636a1
Wandzfuture <michael.zabas@gmail.com>
2024-01-02 12:57:15 -0800

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for numbers in range(0, 100):
-    if numbers == 99:
+for number in range(0, 100):
+    if number == 99:
         print("{}".fotmat(number))
     else:
         print("{:02}".format(number), end=", ")
