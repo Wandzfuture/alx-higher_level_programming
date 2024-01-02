c8501136efa1bd969c1aae3cf6575ce3bde7cd03
Wandzfuture <michael.zabas@gmail.com>
2024-01-02 13:35:00 -0800

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """is a function that prints a string in uppercase."""
-def uppercase(str):
-    for c in str:
+def uppercase(s):
+    result = ""
+    for c in s:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
-            print("{}".format(c), end="")
-            print("")
+        result += c
+    print("{}".format(result))
