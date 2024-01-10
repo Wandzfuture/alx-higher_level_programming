5acb95198c282d42f7822fce925912a6a5728371
Wandzfuture <michael.zabas@gmail.com>
2024-01-10 06:12:06 -0800

    0x04. Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,5 @@
 #1-search_replace.py
 
 def search_replace(my_list, search, replace):
-    def find_search(element):
-        return element if element != search else replace
-    return list(map(find, my_list))
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
