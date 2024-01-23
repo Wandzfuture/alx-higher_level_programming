c1094a7dcf712f7211196072077bdd51a21955ea
Wandzfuture <michael.zabas@gmail.com>
2024-01-23 05:49:48 -0800

    0x06. Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""Empty class Square that defines a square."""
 
 
 class Square:
-    """Empty class Square that defines a square."""
+    """
+    Class that defines properties of square by: (based on 0-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
+    """
 
     def __init__(self, size=0):
         """Initialize the square with a given size.
