98cf2ea0d7fe02c273a2b5a194007d6ef8f62c70
Wandzfuture <michael.zabas@gmail.com>
2024-01-29 13:10:37 -0800

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -4,84 +4,87 @@
 
 
 class Rectangle:
-    """
-    Class that Represents a rectangle.
+    """ Defines a rectangle """
 
-    Attributes:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-        print_symbol (str or any): The symbol used for string representation.
-    """
     number_of_instances = 0
-    print_symbol = "#"
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
-        """Initializes a new Rectangle.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
+    """ WIDTH """
     @property
     def width(self):
-        """int: The width of the rectangle."""
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    """ HEIGHT """
     @property
     def height(self):
-        """int: The height of the rectangle."""
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
+    """ Rectangle area """
     def area(self):
-        """Calculates the area of the rectangle."""
-        return self.__width * self.__height
+        return (self.__height * self.__width)
 
+    """ Rectangle perimeter """
     def perimeter(self):
-        """Calculates the perimeter of the rectangle."""
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
+        if self.width == 0:
+            return (0)
+        if self.height == 0:
+            return (0)
+        return (2 * (self.__height + self.__width))
 
+    """ Returns printable string representation
+    of an instance
+    """
     def __str__(self):
-        """Returns a string representation of the rectangle."""
+        empty_string = ""
+
         if self.__width == 0 or self.__height == 0:
-            return ""
-        return "\n".join([str(self.print_symbol) * self.__width for _ in range(self.__height)])
+            return (empty_string)
 
+        r = (((str(self.print_symbol) * self.width) + "\n") * self.height)[:-1]
+        return (r)
+
+    """ Returns an “official” string representation of an instance """
     def __repr__(self):
-        """Returns a representation of the rectangle."""
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
 
+    """ Instance method called when an instance is deleted """
     def __del__(self):
-        """Destructor for Rectangle."""
-        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
 
-    @staticmethod
+    """ Returns the biggest rectangle based on the area """
     def bigger_or_equal(rect_1, rect_2):
-        """Returns the bigger rectangle based on the area."""
-        if not isinstance(rect_1, Rectangle):
+
+        if isinstance(rect_1, Rectangle) is not True:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
+        if isinstance(rect_2, Rectangle) is not True:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
+
+        if rect_1.area() == rect_2.area():
+            return (rect_1)
+
+        if rect_2.area() > rect_1.area():
+            return (rect_2)
+        else:
+            return (rect_1)
