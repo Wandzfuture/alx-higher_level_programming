8f2094a78abd5eb3343a815a717b3550f20f1484
Wandzfuture <michael.zabas@gmail.com>
2024-01-31 09:48:48 -0800

    0x09. Python - Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def magic_string():
-    if not hasattr(magic_string, "count"):
-        magic_string.count = 1
-    else:
-        magic_string.count += 1
-    return "BestSchool" * magic_string.count
+def magic_string(n=1):
+    result = []
+    for i in range(n):
+        if i == 0:
+            result.append('BestSchool')
+        else:
+            result.append(f', BestSchool')
+    return ''.join(result)
