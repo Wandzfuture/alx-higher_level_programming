8ffb3e3b22600945225db184b2e879b1c36c42a4
Wandzfuture <michael.zabas@gmail.com>
2024-02-05 03:40:43 -0800

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,36 +1,53 @@
 #!/usr/bin/python3
-"""Square class Module"""
-Rectangle = __import__("9-rectangle").Rectangle
+"""
+Contains the class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator."""
+    def area(self):
+        """Raises an exception when called."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that value is an integer greater than 0."""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
-    """
-    A class representing a rectangle.
-    """
-
+    """A representation of a rectangle."""
     def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance with the specified width and height.
-
-        Args:
-            width: The width of the rectangle (positive integer).
-            height: The height of the rectangle (positive integer).
-        """
+        """Instantiation of the rectangle."""
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
 
     def area(self):
-        """
-        Calculates and returns the area of the rectangle.
-
-        Returns:
-            The area of the rectangle (width * height).
-        """
+        """Returns the area of the rectangle."""
         return self.__width * self.__height
 
     def __str__(self):
-        """
-        Returns a string representation of the Rectangle instance.
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+        """Informal string representation of the rectangle."""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """A representation of a square."""
+    def __init__(self, size):
+        """Instantiation of the square."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Informal string representation of the square."""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
