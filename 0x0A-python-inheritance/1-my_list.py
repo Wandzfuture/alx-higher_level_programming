a3ec36af905c2ed1047b31c0a6b3c966f50cc3c3
Wandzfuture <michael.zabas@gmail.com>
2024-02-05 03:05:33 -0800

    0x0A. Python - Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""A module to prints a list in ascending order
-"""
+"""A module to print a list in ascending order"""
 
 class MyList(list):
-    """
-    A custom list class that inherits from the built-in list class.
-    """
+    """A custom list class that inherits from the built-in list class."""
 
     def print_sorted(self):
         """Prints a list in ascending order."""
