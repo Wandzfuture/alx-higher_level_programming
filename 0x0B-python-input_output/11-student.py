b94c573e401d53dbaba39cd1117d603a3e9cd9e9
Wandzfuture <michael.zabas@gmail.com>
2024-02-07 02:45:06 +0000

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""Defines a class Student.
+"""
+
+
+class Student:
+    """Defines a student with first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance with the given attributes."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns a dictionary representation of the Student instance.
+
+        Args:
+            attrs (list, optional): List of attribute names to retrieve. If None, all attributes are retrieved.
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age
+            }
+        else:
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs
+                if hasattr(self, attr)
+            }
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance with values from a dictionary.
+
+        Args:
+            json (dict): Dictionary containing attribute names and values to reload.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
